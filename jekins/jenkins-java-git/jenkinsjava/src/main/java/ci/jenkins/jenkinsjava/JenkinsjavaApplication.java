1a4f9de015a07da42127ef63df89eaec67688c2e
ncho23 <isaacncho9@gmail.com>
2023-11-07 09:10:56 +0000

    spring boot init

---
+++ jekins/jenkins-java-git/jenkinsjava/src/main/java/ci/jenkins/jenkinsjava/JenkinsjavaApplication.java
@@ -0,0 +1,13 @@
+package ci.jenkins.jenkinsjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsjavaApplication.class, args);
+	}
+
+}
---
+++ jekins/jenkins-java-git/jenkinsjava/src/test/java/ci/jenkins/jenkinsjava/JenkinsjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package ci.jenkins.jenkinsjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
